4317fbf4e8c6e8a03e5ec648fd58cd6ee8ec21ee
arun13-bisht <bishtarun13@gmail.com>
2026-06-09 15:33:33 +0530

    Add Terraform outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "resource_group_name" {
+  description = "Azure resource group ka naam"
+  value = var.resource_group_name
+}
+
+output "location" {
+  description = "Azure region"
+  value = var.location
+}
