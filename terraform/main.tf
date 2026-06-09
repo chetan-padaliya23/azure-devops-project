be450a578bbec01c7ef01ce2a3d4d55b0eaa7025
arun13-bisht <bishtarun13@gmail.com>
2026-06-09 16:02:45 +0530

    Add VNet and Subnet resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,18 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-devops-project"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-devops-project"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
