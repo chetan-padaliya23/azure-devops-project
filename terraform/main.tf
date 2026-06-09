ebe7b3ebd0ec0ec5c592c9a81d1d3187b958ebaa
arun13-bisht <bishtarun13@gmail.com>
2026-06-09 16:18:26 +0530

    Add Public IP and Network Interface resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,3 +31,23 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "public-ip-devops-project"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-devops-project"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig-devops-project"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
