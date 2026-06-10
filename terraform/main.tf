ba966bb0099225df2a1cc8c2bdec8fee7d6b0a91
arun13-bisht <bishtarun13@gmail.com>
2026-06-10 11:42:00 +0530

    Add NSG with SSH rule and NIC association

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,3 +79,27 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-devops-project"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "subnet_nsg_assoc" {
+  network_interface_id       = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+  
+}
